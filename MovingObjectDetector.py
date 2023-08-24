ed927d963738af981dc2111f83b69ba57592d9a6
Abhinandan Singh <singhabhinandan614@gmail.com>
2023-08-24 09:58:57 +0530

    Created background subtraction and performed moving object detection

--- MovingObjectDetector.py
+++ MovingObjectDetector.py
@@ -4,13 +4,29 @@ def main():
     # Create video capture object
     video_capture = cv2.VideoCapture(0)  # Use 0 for the default camera 
 
+    # Create background subtractor
+    bg_subtractor = cv2.createBackgroundSubtractorMOG2()
+    
     while True:
         ret, frame = video_capture.read()
         if not ret:
             break
+        
+        # Apply background subtraction
+        fg_mask = bg_subtractor.apply(frame)
 
-        # Display the frame
-        cv2.imshow("Video", frame)
+        # Find contours of moving objects
+        contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+        # Draw rectangles around moving objects
+        for contour in contours: 
+            # check if the area of contour is greater than 400
+            if cv2.contourArea(contour) > 400: 
+                x, y, w, h = cv2.boundingRect(contour)
+                cv2.rectangle(frame,(x,y), (x+w, y+h), (0,255,0))
+
+        # Display the frame with moving object detection
+        cv2.imshow("Moving Object Detector", frame)
         
         # Press 'q' to exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
